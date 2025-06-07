4f5e1fddc9a466233930e6785e5579ec89946d3a
anuja-28 <anuamale19@gmail.com>
2025-06-07 23:54:14 +0530

    commit on add ustomer impl repo

---
+++ demo/src/main/java/com/example/demo/repository/CustomerImplRepo.java
@@ -0,0 +1,24 @@
+package com.example.demo.repository;
+
+// import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.demo.model.Customer;
+
+@Repository
+public class CustomerImplRepo implements CustomerRepository {
+ 
+ @Autowired
+ JdbcTemplate jdbcTemplate;
+ 
+ @Override
+ public int addCustomer(Customer customer) {
+  String insertQuery = "insert into tbl_customer_details(cust_name, cust_email) values (?,?)";
+  return jdbcTemplate.update(insertQuery,customer.getCustName(),customer.getCustEmail());
+ }
+
+}
--- demo/src/main/java/com/example/demo/repository/CustomerRepository.java
+++ demo/src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -1,5 +1,16 @@
 package com.example.demo.repository;
 
-public class CustomerRepository {
+// import java.util.List;
+
+import com.example.demo.model.Customer;
+
+//its an interface the implementation will be in the implemeting repository
+public interface CustomerRepository {
+
+ int addCustomer(Customer customer);
+//  List<Customer> getAllCustomers();
+//  Customer getCustomerById(int cust_id);
+//  int updateCustomer(Customer customer);
+//  int deleteCustomerById(int cust_id);
     
 }
