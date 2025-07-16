f90e81b9225d7d99fedd8dbef0cb1edf2f85a81b
anuja-28 <anuamale19@gmail.com>
2025-07-16 21:25:22 +0530

    commit on fetched ajax table

--- demo/src/main/java/com/example/demo/controller/CustomerController.java
+++ demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -2,13 +2,15 @@ package com.example.demo.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
@@ -36,7 +38,14 @@ public class CustomerController {
         customerRepository.addCustomer(cust);
 
         return ResponseEntity.ok("Customer saved successfully");
+    }  
+
+    @GetMapping("/getcustomers")
+    @ResponseBody
+    public List<Customer> getCustomers() {
+        return customerRepository.getAllCustomers();
     }
+
 
 
 
